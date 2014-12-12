4bbd119887575e460c352b3d241d7f819fbaa39f
Brice Videau <brice.videau@imag.fr>
2014-12-12 16:14:51 +0100

    Refactoring.

--- lib/BOAST/OpenMP.rb
+++ lib/BOAST/OpenMP.rb
@@ -16,58 +16,66 @@ module BOAST
         return s
       end
 
-      def openmp_clauses_to_s
+      # Registers an openmp clause, arg_type can be :none, :simple, :list, :multilist
+      def self.register_clause( name, arg_type )
+        s = <<EOF
+      def openmp_clause_#{name}(c)
+EOF
+        case arg_type
+        when :none
+          s += <<EOF
+        return " #{name}"
+EOF
+        when :simple
+          s += <<EOF
+        return " #{name}(\#{c})"
+EOF
+        when :list
+s += <<EOF
+        return " #{name}(\#{[c].flatten.join(", ")})"
+EOF
+        when :multilist
+s += <<EOF
         s = ""
-        if @openmp_clauses[:if] then
-          s += " if(#{@openmp_clauses[:if]})"
+        c.each { |id, list|
+          s += " #{name}(\#{id}: \#{[list].flatten.join(", ")})"
+        }
+        return s
+EOF
+        else
+          raise "Unknown argument type!"
         end
-        if @openmp_clauses[:num_threads] then
-          s += " num_threads(#{@openmp_clauses[:num_threads]})"
-        end
-        if @openmp_clauses[:default] then
-          s += " default(#{@openmp_clauses[:default]})"
-        end
-        if @openmp_clauses[:private] then
-          s += " private(#{[@openmp_clauses[:private]].flatten.join(", ")})"
-        end
-        if @openmp_clauses[:firstprivate] then
-          s += " firstprivate(#{[@openmp_clauses[:firstprivate]].flatten.join(", ")})"
-        end
-        if @openmp_clauses[:lastprivate] then
-          s += " lastprivate(#{[@openmp_clauses[:lastprivate]].flatten.join(", ")})"
-        end
+        s += <<EOF
+      end
+EOF
+        eval s
+      end
+
+      register_clause(:nowait,       :none)
+      register_clause(:ordered,      :none)
+      register_clause(:if,           :simple)
+      register_clause(:num_threads,  :simple)
+      register_clause(:default,      :simple)
+      register_clause(:collapse,     :simple)
+      register_clause(:private,      :list)
+      register_clause(:shared,       :list)
+      register_clause(:firstprivate, :list)
+      register_clause(:lastprivate,  :list)
+      register_clause(:copyprivate,  :list)
+      register_clause(:copyin,       :list)
+      register_clause(:schedule,     :list)
+      register_clause(:reduction,    :multilist)
+      register_clause(:depend,       :multilist)
+
+      def openmp_open_clauses_to_s
+        s = ""
+        get_open_clauses.each { |c|
+          s += self.send( "openmp_clause_#{c}", @openmp_clauses[c] ) if @openmp_clauses[c]
+        }
         if lang == C then
-          if @openmp_clauses[:copyprivate] then
-            s += " copyprivate(#{[@openmp_clauses[:copyprivate]].flatten.join(", ")})"
-          end
-          if @openmp_clauses[:nowait] then
-            s += " nowait"
-          end
-        end
-        if @openmp_clauses[:shared] then
-          s += " shared(#{[@openmp_clauses[:shared]].flatten.join(", ")})"
-        end
-        if @openmp_clauses[:copyin] then
-          s += " copyin(#{[@openmp_clauses[:copyin]].flatten.join(", ")})"
-        end
-        if @openmp_clauses[:reduction] then
-          @openmp_clauses[:reduction].each { |identifier, list|
-            s += " reduction(#{identifier}: #{list.join(", ")})"
+          get_end_clauses.each { |c|
+            s += self.send( c, @openmp_clauses[c] ) if @openmp_clauses[c]
           }
-        end
-        if @openmp_clauses[:depend] then
-          @openmp_clauses[:depend].each { |type, list|
-            s += " depend(#{type}: #{list.join(", ")})"
-          }
-        end
-        if @openmp_clauses[:schedule] then
-          s += " schedule(#{@openmp_clauses[:schedule].join(", ")})"
-        end
-        if @openmp_clauses[:collapse] then
-          s += " collapse(#{@openmp_clauses[:collapse]})"
-        end
-        if @openmp_clauses[:ordered] then
-          s += " ordered"
         end
         return s
       end
@@ -75,12 +83,9 @@ module BOAST
       def openmp_end_clauses_to_s
         s = ""
         if lang == FORTRAN then
-          if @openmp_clauses[:copyprivate] then
-            s += " copyprivate(#{[@openmp_clauses[:copyprivate]].flatten.join(", ")})"
-          end
-          if @openmp_clauses[:nowait] then
-            s += " nowait"
-          end
+          get_end_clauses.each { |c|
+            s += self.send( c, @openmp_clauses[c] ) if @openmp_clauses[c]
+          }
         end
         return s
       end
@@ -117,7 +122,7 @@ EOF
       eval token_string_generator( * %w{end c})
 
       def to_s
-        return begin_string(openmp_clauses_to_s)
+        return begin_string(openmp_open_clauses_to_s)
       end
 
       def open
@@ -153,6 +158,26 @@ EOF
                  :end => '"!$omp end parallel #{c}"' }
       end
 
+      def self.get_open_clauses
+        return [ :if,
+                 :num_threads,
+                 :default,
+                 :private,
+                 :firstprivate,
+                 :shared,
+                 :copyin,
+                 :reduction,
+                 :proc_bind ]
+      end
+
+      def get_open_clauses
+        return Parallel.get_open_clauses
+      end
+
+      def get_end_clauses
+        return [ ]
+      end
+
     end
 
     class For < OpenMPControlStructure
@@ -166,6 +191,26 @@ EOF
         return { :begin => '"!$omp do #{c}"',
                  :end => '"!$omp end do #{c}"' }
       end
+
+      def self.get_open_clauses
+        return [ :private,
+                 :firstprivate,
+                 :lastprivate,
+                 :reduction,
+                 :schedule,
+                 :collapse,
+                 :ordered ]
+      end
+
+      def get_open_clauses
+        return For.get_open_clauses
+      end
+
+      def get_end_clauses
+        return [ :nowait ]
+      end
+
+    end
 
     end
 
