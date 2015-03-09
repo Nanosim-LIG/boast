e4b0e4282aa61464b8de599696cd24d394c38cf3
Brice Videau <brice.videau@imag.fr>
2015-03-09 15:32:43 +0100

    Reworked verctor_add sample.

--- sample/BOAST_vector_add.rb
+++ sample/BOAST_vector_add.rb
@@ -1,39 +1,33 @@
 require 'narray'
 require 'BOAST'
-module BOAST
-  def BOAST::vector_add
-    kernel = CKernel::new
-    function_name = "vector_add"
-    n = Int("n",{:dir => :in, :signed => false})
-    a = Real("a",{:dir => :in, :dim => [ Dim(0,n-1)] })
-    b = Real("b",{:dir => :in, :dim => [ Dim(0,n-1)] })
-    c = Real("c",{:dir => :out, :dim => [ Dim(0,n-1)] })
-    i = Int :i, :signed => false 
-    ig = Sizet :ig
-    if get_lang == CL then
-      @@output.puts "#pragma OPENCL EXTENSION cl_khr_fp64: enable"
-    end
-    print p = Procedure(function_name, [n,a,b,c]) {
-      if (get_lang == CL or get_lang == CUDA) then
-        decl ig
-        print ig === get_global_id(0)
-        print c[ig] === a[ig] + b[ig]
-      else
-        decl i
-        print For(i,0,n-1) {
-          print c[i] === a[i] + b[i]
-        }
-      end
-    }
-    kernel.procedure = p
-    return kernel
+include BOAST
+set_array_start( 0 )
+def vector_add
+  kernel = CKernel::new
+  n = Int("n",{:dir => :in, :signed => false})
+  a = Real("a",{:dir => :in, :dim => [ Dim(n)] })
+  b = Real("b",{:dir => :in, :dim => [ Dim(n)] })
+  c = Real("c",{:dir => :out, :dim => [ Dim(n)] })
+  i = Int( :i, :signed => false )
+  ig = Sizet :ig
+  if get_lang == CL then
+    @@output.puts "#pragma OPENCL EXTENSION cl_khr_fp64: enable"
   end
+  pr p = Procedure("vector_add", [n,a,b,c]) {
+    if (get_lang == CL or get_lang == CUDA) then
+      decl ig
+      pr ig === get_global_id(0)
+      pr c[ig] === a[ig] + b[ig]
+    else
+      decl i
+      pr For(i,0,n-1) {
+        pr c[i] === a[i] + b[i]
+      }
+    end
+  }
+  kernel.procedure = p
+  return kernel
 end
-
-def rndup( val, div)
-  return (val%div) == 0 ? val : val + div - (val%div)
-end
-
 
 n = 1024*1024
 a = NArray.float(n).random
@@ -43,28 +37,20 @@ c_ref = NArray.float(n)
 
 epsilon = 10e-15
 
-BOAST::set_lang( BOAST::FORTRAN )
-puts "FORTRAN"
-k = BOAST::vector_add
-puts k.print
-k.run(n,a,b,c_ref)
-BOAST::set_lang( BOAST::C )
-puts "C"
-c.random
-k = BOAST::vector_add
-puts k.print
-k.run(n,a,b,c)
-diff = (c_ref - c).abs
-diff.each { |elem|
-  raise "Warning: residue too big: #{elem}" if elem > epsilon
-}
-BOAST::set_lang( BOAST::CL )
-puts "CL"
-c.random
-k = BOAST::vector_add
-puts k.print
-k.run(n, a, b, c, :global_work_size => [rndup(n,32), 1,1], :local_work_size => [32,1,1] )
-diff = (c_ref - c).abs
-diff.each { |elem|
-  raise "Warning: residue too big: #{elem}" if elem > epsilon
+c_ref = a + b
+
+[:FORTRAN, :C, :CL, :CUDA].each { |l|
+  set_lang( BOAST.const_get(l)  )
+  puts l
+  k = vector_add
+  puts k.print
+  if lang == CL or lang == CUDA then
+    k.run(n, a, b, c, :global_work_size => [n,1,1], :local_work_size => [32,1,1])
+  else
+    k.run(n, a, b, c)
+  end
+  diff = (c_ref - c).abs
+  diff.each { |elem|
+    raise "Warning: residue too big: #{elem}" if elem > epsilon
+  }
 }
