a1f9e79230b300474f9d5c53b351297705f99349
Brice Videau <brice.videau@imag.fr>
2015-11-24 16:53:27 +0100

    WIP: refactoring Optimization framework.

--- lib/BOAST/Language/Optimization.rb
+++ lib/BOAST/Language/Optimization.rb
@@ -17,6 +17,67 @@ module BOAST
 
   OP = OptimizationParameter
   BP = BooleanParameter
+
+  class OptimizationSpace
+    attr_reader :parameters
+
+    def initialize( *parameters )
+      if parameters.length == 1 and parameters[0].is_a?(Hash) then
+        @parameters = []
+        parameters[0].each { |key, value|
+          @parameters.push( OptimizationParameter::new(key, value) )
+        }
+      else
+        @parameters = parameters
+      end
+    end
+
+  end
+
+  class BruteForceOptimizer
+    def initialize(search_space, options = {} )
+      @search_space = search_space
+      @randomize = options[:randomize]
+    end
+
+    def points
+      params2 = @search_space.parameters.dup
+      param = params2.shift
+      pts = param.values.collect { |val| {param.name => val} }
+      if params2.size == 0 then
+        return pts
+      else
+        optim2 = BruteForceOptimizer::new(OptimizationSpace::new(*params2))
+        pts3=[]
+        pts.each{ |p1|
+          optim2.each { |p2|
+            pts3.push(p1.dup.update(p2))
+          }
+        }
+        return pts3
+      end
+    end
+
+    def each(&block)
+      return self.points.each(&block)
+    end
+
+    def each_random(&block)
+      return self.points.shuffle.each(&block)
+    end
+
+    def optimize(&block)
+      best = [nil, Float::INFINITY]
+      pts = points
+      pts.shuffle! if @randomize
+      enumerator = pts.each { |config|
+        metric = block.call(config)
+        best = [config, metric] if metric < best[1]
+      }
+      return best
+    end
+
+  end
  
   class GenericOptimization
 
@@ -29,19 +90,16 @@ module BOAST
     end
  
     def points
-      pts=[]
       params2 = @parameters.dup
       param = params2.shift
-      optim2 = GenericOptimization::new(*params2)
-      param.values.each{ |val| 
-        pts.push({param.name.to_sym => val})
-      }
-      if optim2.size == 0 then
+      pts = param.values.collect { |val| {param.name => val} }
+      if params2.size == 0 then
         return pts
       else
+        optim2 = GenericOptimization::new(*params2)
         pts3=[]
-        pts.each{ |p1| 
-          optim2.each { |p2| 
+        pts.each{ |p1|
+          optim2.each { |p2|
             pts3.push(p1.dup.update(p2))
           }
         }
