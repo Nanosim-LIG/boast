44c08633c9a3d5565e81c349656bc1e75852af30
Brice Videau <brice.videau@imag.fr>
2015-12-14 11:56:41 +0100

    Reduce the number of generations and use MutativeReproduction.

--- optimizer_benchmarks/bench_optimizer.rb
+++ optimizer_benchmarks/bench_optimizer.rb
@@ -12,7 +12,7 @@ puts optimizer.optimize { |opts|
 puts optimizer.experiments
 
 optimizer = GeneticOptimizer::new( opt_space )
-puts optimizer.optimize { |opts|
+puts optimizer.optimize(:generations_limit => 10, :evolution_types => [Darwinning::EvolutionTypes::MutativeReproduction.new(mutation_rate: 0.10) ]) { |opts|
   oracle[opts]*1000000000
 } 
 puts optimizer.experiments
