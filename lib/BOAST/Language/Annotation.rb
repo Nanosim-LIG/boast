9b674329176baa59a23605d1dcd5b11afad7162c
Brice Videau <brice.videau@imag.fr>
2016-01-20 14:59:39 +0100

    Smarter annotations...

--- lib/BOAST/Language/Annotation.rb
+++ lib/BOAST/Language/Annotation.rb
@@ -14,16 +14,36 @@ module BOAST
       return "#{name}#{annotate_number(name)}"
     end
 
+    def annotate_scalar(s, level)
+      if s.is_a?(Annotation) and s.annotate_indepth?(level + 1) then
+        return { s.annotation_identifier => s.annotation(level + 1) }
+      elsif s.is_a?(Numeric)
+        return s
+      else
+        return s.to_s
+      end
+    end
+
+    def annotate_array(a, level)
+      return a.collect { |e|
+        annotate_var(e, level)
+      }
+    end
+
+    def annotate_var(v, level)
+      if v.is_a?(Array) then
+        return annotate_array(v, level)
+      else
+        return annotate_scalar(v, level)
+      end
+    end
+
     def annotation(level)
       anns = {}
       self.class.const_get(:ANNOTATIONS).each { |a|
         var_sym = ("@" + a.to_s).to_sym
         var = self.instance_variable_get(var_sym)
-        if var.is_a?(Annotation) and var.annotate_indepth?(level + 1) then
-          anns[a] = { var.annotation_identifier => var.annotation(level + 1) }
-        else
-          anns[a] = var.to_s
-        end
+        anns[a] = annotate_var(var, level)
       }
       return anns
     end
