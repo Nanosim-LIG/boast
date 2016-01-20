1b703c0c333cbe68961cb0536d4d307bb855ba3a
Brice Videau <brice.videau@imag.fr>
2016-01-20 16:52:39 +0100

    Can go inside C resulting expression of Dimension.

--- lib/BOAST/Language/Annotation.rb
+++ lib/BOAST/Language/Annotation.rb
@@ -21,7 +21,7 @@ module BOAST
         return s
       else
         str = s.to_s
-        str = str.to_s unless str.is_a?(String) #Dimension.to_s can return something else than a String... FIXME
+        str = annotate_var(str, level) unless str.is_a?(String) #Dimension.to_s can return something else than a String... FIXME
         str = nil if str == ''
         return str
       end
