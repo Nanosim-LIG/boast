633e84b3b3d613fddc6fc915b00369e7844e5412
Brice Videau <brice.videau@imag.fr>
2016-01-26 11:32:00 +0100

    Test re-factoring.

--- test/BOAST_Language_test.rb
+++ test/BOAST_Language_test.rb
@@ -3,125 +3,97 @@ require 'minitest/autorun'
 require 'BOAST'
 include BOAST
 
+module MiniTest::Assertions
+
+  def assert_subprocess_output( stdout = nil, stderr = nil)
+    out, err = capture_subprocess_io do
+      yield
+    end
+
+    err_msg = Regexp === stderr ? :assert_match : :assert_equal if stderr
+    out_msg = Regexp === stdout ? :assert_match : :assert_equal if stdout
+
+    y = send err_msg, stderr, err, "In stderr" if err_msg
+    x = send out_msg, stdout, out, "In stdout" if out_msg
+
+    (!stdout || x) && (!stderr || y)
+  end
+
+end
+
 class TestLanguage < Minitest::Test
 
   def test_decl_int
+    block = lambda { decl Int(:a) }
     set_lang(FORTRAN)
-    out, err = capture_subprocess_io do
-      decl Int(:a)
-    end
-    assert_equal(out, "integer(kind=4) :: a\n")
-    assert_equal(err, "")
+    assert_subprocess_output( "integer(kind=4) :: a\n", "", &block )
     set_lang(C)
-    out, err = capture_subprocess_io do
-      decl Int(:a)
-    end
-    assert_equal(out, "int32_t a;\n")
-    assert_equal(err, "")
+    assert_subprocess_output( "int32_t a;\n", "", &block )
   end
 
   def test_decl_int_64
-    set_lang(FORTRAN)
     begin
       push_env(:default_int_size => 8)
-      out, err = capture_subprocess_io do
-        decl Int(:a)
-      end
-      assert_equal(out, "integer(kind=8) :: a\n")
-      assert_equal(err, "")
+      block = lambda { decl Int(:a) }
+      set_lang(FORTRAN)
+      assert_subprocess_output( "integer(kind=8) :: a\n", "", &block )
       set_lang(C)
-      out, err = capture_subprocess_io do
-        decl Int(:a)
-      end
-      assert_equal(out, "int64_t a;\n")
-      assert_equal(err, "")
+      assert_subprocess_output( "int64_t a;\n", "", &block )
     ensure
       pop_env(:default_int_size)
     end
   end
 
   def test_decl_int_array
-    set_lang(FORTRAN)
     n1 = Int("n1")
     arr = Int(:a, :dim => [Dim(5,15), Dim(n1)])
-    out, err = capture_subprocess_io do
-      decl arr
-    end
-    assert_equal(out, "integer(kind=4), dimension(5:15, n1) :: a\n")
-    assert_equal(err, "")
+    block = lambda { decl arr }
+    set_lang(FORTRAN)
+    assert_subprocess_output( "integer(kind=4), dimension(5:15, n1) :: a\n", "", &block )
     set_lang(C)
-    out, err = capture_subprocess_io do
-      decl arr
+    assert_subprocess_output( "int32_t * a;\n", "", &block )
+    begin
+      push_env(:use_vla => true)
+      assert_subprocess_output( "int32_t a[n1][11];\n", "", &block )
+    ensure
+      pop_env(:use_vla)
     end
-    assert_equal(out, "int32_t * a;\n")
-    assert_equal(err, "")
-    push_env(:use_vla => true)
-    out, err = capture_subprocess_io do
-      decl arr
-    end
-    assert_equal(out, "int32_t a[n1][11];\n")
-    assert_equal(err, "")
-    pop_env(:use_vla)
   end
 
   def test_decl_int_array_unkwown_dim
-    set_lang(FORTRAN)
     arr = Int(:a, :dim => [Dim(5,15), Dim()])
-    out, err = capture_subprocess_io do
-      decl arr
-    end
-    assert_equal(out, "integer(kind=4), dimension(5:15, *) :: a\n")
-    assert_equal(err, "")
+    block = lambda { decl arr }
+    set_lang(FORTRAN)
+    assert_subprocess_output( "integer(kind=4), dimension(5:15, *) :: a\n", "", &block )
     set_lang(C)
-    out, err = capture_subprocess_io do
-      decl arr
-    end
-    assert_equal(out, "int32_t * a;\n")
-    assert_equal(err, "")
+    assert_subprocess_output( "int32_t * a;\n", "", &block )
     begin
       push_env(:use_vla => true)
-      out, err = capture_subprocess_io do
-        decl arr
-      end
-      assert_equal(out, "int32_t a[][11];\n")
-      assert_equal(err, "")
+      assert_subprocess_output( "int32_t a[][11];\n", "", &block )
     ensure
       pop_env(:use_vla)
     end
   end
 
   def test_decl_int_array_deffered_shape
-    set_lang(FORTRAN)
     arr = Int(:a, :dim => [Dim(5,15), Dim()], :deferred_shape => true)
-    out, err = capture_subprocess_io do
+    set_lang(FORTRAN)
+    assert_subprocess_output( "integer(kind=4), dimension(:, :) :: a\n", "" ) do
       decl arr
     end
-    assert_equal(out, "integer(kind=4), dimension(:, :) :: a\n")
-    assert_equal(err, "")
   end
 
   def test_pr_int_array_index
-    set_lang(FORTRAN)
     n1 = Int("n1")
     arr = Int(:a, :dim => [Dim(5,15), Dim(n1)])
-    out, err = capture_subprocess_io do
-      pr arr[6,7]
-    end
-    assert_equal(out, "a(6, 7)\n")
-    assert_equal(err, "")
+    block = lambda { pr arr[6,7] }
+    set_lang(FORTRAN)
+    assert_subprocess_output( "a(6, 7)\n", "", &block )
     set_lang(C)
-    out, err = capture_subprocess_io do
-      pr arr[6,7]
-    end
-    assert_equal(out, "a[6 - (5) + (11) * (7 - (1))];\n")
-    assert_equal(err, "")
+    assert_subprocess_output( "a[6 - (5) + (11) * (7 - (1))];\n", "", &block )
     begin
       push_env(:use_vla => true)
-      out, err = capture_subprocess_io do
-        pr arr[6,7]
-      end
-      assert_equal(out, "a[7 - (1)][6 - (5)];\n")
-      assert_equal(err, "")
+      assert_subprocess_output( "a[7 - (1)][6 - (5)];\n", "", &block )
     ensure
       pop_env(:use_vla)
     end
@@ -129,26 +101,15 @@ class TestLanguage < Minitest::Test
 
   def test_pr_int_array_index_unkwown_dim
     n1 = Int("n1")
-    set_lang(FORTRAN)
     arr = Int(:a, :dim => [Dim(n1), Dim()])
-    out, err = capture_subprocess_io do
-      pr arr[6,7]
-    end
-    assert_equal(out, "a(6, 7)\n")
-    assert_equal(err, "")
+    block = lambda { pr arr[6,7] }
+    set_lang(FORTRAN)
+    assert_subprocess_output( "a(6, 7)\n", "", &block )
     set_lang(C)
-    out, err = capture_subprocess_io do
-      pr arr[6,7]
-    end
-    assert_equal(out, "a[6 - (1) + (n1) * (7 - (1))];\n")
-    assert_equal(err, "")
+    assert_subprocess_output( "a[6 - (1) + (n1) * (7 - (1))];\n", "", &block )
     begin
       push_env(:use_vla => true, :array_start => 0)
-      out, err = capture_subprocess_io do
-        pr arr[6,7]
-      end
-      assert_equal(out, "a[7][6];\n")
-      assert_equal(err, "")
+      assert_subprocess_output( "a[7][6];\n", "", &block )
     ensure
       pop_env(:use_vla, :array_start)
     end
