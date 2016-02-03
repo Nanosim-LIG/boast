bf0d042d5212ee6765743e0b3fca244acbed3e47
Brice Videau <brice.videau@imag.fr>
2016-02-03 11:25:12 +0100

    Improved/more flexible Case.

--- lib/BOAST/Language/Case.rb
+++ lib/BOAST/Language/Case.rb
@@ -1,39 +1,96 @@
 module BOAST
 
+  class CaseCondition < ControlStructure
+    attr_reader :block
+    attr_reader :constants
+
+    def initialize(constants = nil, &block)
+      @constants = constants
+      @block = block
+    end
+
+    def get_c_strings
+      return { :case => '"case #{constants.join(" : case ")} :"',
+               :default => '"default :"',
+               :break => '"break;"' }
+    end
+
+    def get_fortran_strings
+      return { :case => '"case (#{constants.join(", ")})"',
+               :default => '"case default"',
+               :break => 'nil' }
+    end
+
+    alias get_cl_strings get_c_strings
+    alias get_cuda_strings get_c_strings
+
+    eval token_string_generator( * %w{case constants})
+    eval token_string_generator( * %w{default})
+    eval token_string_generator( * %w{break})
+
+    def to_s
+      s = ""
+      if @constants then
+        s += case_string(@constants)
+      else
+        s += default_string
+      end
+      return s
+    end
+
+    def open
+      s = ""
+      s += indent
+      s += to_s
+      output.puts s
+      increment_indent_level
+      return self
+    end
+
+    def close
+      if @constants and break_string then
+        s = ""
+        s += indent
+        s += break_string
+        output.puts s
+      end
+      decrement_indent_level
+      return self
+    end
+
+    def pr(*args)
+      open
+      if @block then
+        @block.call(*args)
+        close
+      end
+      return self
+    end
+
+  end
+
   class Case < ControlStructure
 
     attr_reader :expression
-    attr_reader :constants_list
+    attr_reader :case_conditions
 
     def initialize(expression, *control, &block)
       @expression = expression
-      @constants_list = []
-      @blocks = []
+      @case_conditions = []
       control.push(block) if block
-      if control.size < 1 then
-        raise "No block given!"
-      else
-        while control.size >= 2 do
-          @constants_list.push [control.shift].flatten
-          @blocks.push control.shift
-        end
-        @blocks.push control.shift if control.size > 0
+      while control.size >= 2 do
+        @case_conditions.push CaseCondition([control.shift].flatten, &(control.shift))
       end
+      @case_conditions.push CaseCondition(&(control.shift)) if control.size > 0
     end
 
     def get_c_strings
       return { :switch => '"switch (#{expr}) {"',
-               :case => '"case #{constants.join(" : case ")} :"',
-               :default => '"default :"',
-               :break => '"break;"',
                :end => '"}"' }
     end
 
     def get_fortran_strings
       return { :switch => '"select case (#{expr})"',
-               :case => '"case (#{constants.join(", ")})"',
-               :default => '"case default"',
-               :break => 'nil',
                :end => '"end select"' }
     end
 
@@ -41,30 +98,12 @@ module BOAST
     alias get_cuda_strings get_c_strings
 
     eval token_string_generator( * %w{switch expr})
-    eval token_string_generator( * %w{case constants})
-    eval token_string_generator( * %w{default})
-    eval token_string_generator( * %w{break})
     eval token_string_generator( * %w{end})
 
     def to_s
       s = ""
       s += switch_string(@expression)
       return s
-    end
-
-    def pr_block(block_number, *args)
-      c = @constants_list[block_number] and @constants_list[block_number].size > 0
-      if c then
-        output.puts indent + case_string(@constants_list[block_number])
-      else
-        output.puts indent + default_string
-      end
-      increment_indent_level
-      @blocks[block_number].call(*args)
-      if c then
-        output.puts indent + break_string + "\n" if break_string
-      end
-      decrement_indent_level
     end
 
     def open
@@ -78,9 +117,9 @@ module BOAST
 
     def pr(*args)
       open
-      if @blocks.size > 0 then
-        @blocks.each_index { |indx|
-          pr_block(indx, *args)
+      if @case_conditions.size > 0 then
+        @case_conditions.each { |cond|
+          cond.pr(*args)
         }
         close
       end
@@ -88,7 +127,7 @@ module BOAST
     end
 
     def close
-      decrement_indent_level      
+      decrement_indent_level
       s = ""
       s += indent
       s += end_string
