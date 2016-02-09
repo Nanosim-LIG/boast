9c53379f052738dd167ed399e051d68572697258
Brice Videau <brice.videau@imag.fr>
2016-02-09 14:31:36 +0100

    More array tests.

--- test/Language/Array.rb
+++ test/Language/Array.rb
@@ -113,4 +113,50 @@ class TestArray < Minitest::Test
     end
   end
 
+  def test_decl_int_array_local
+    n1 = Int("n1")
+    arr = Int(:a, :dim => [Dim(5,15), Dim(n1)], :local => true)
+    block = lambda { decl arr }
+    set_lang(FORTRAN)
+    assert_subprocess_output( "integer(kind=4), dimension(5:15, n1) :: a\n", "", &block )
+    set_lang(C)
+    assert_subprocess_output( "int32_t a[(n1)*(11)];\n", "", &block )
+    set_lang(CL)
+    assert_subprocess_output( "__local int a[(n1)*(11)];\n", "", &block )
+    set_lang(CUDA)
+    assert_subprocess_output( "__shared__ int a[(n1)*(11)];\n", "", &block )
+  end
+
+  def test_decl_int_array_allocate
+    n1 = Int("n1")
+    arr = Int(:a, :dim => [Dim(5,15), Dim(n1)], :allocate => :heap)
+    block = lambda { decl arr }
+    block2 = lambda { pr arr.alloc }
+    block3 = lambda { pr arr.dealloc }
+    set_lang(FORTRAN)
+    assert_subprocess_output( "integer(kind=4), allocatable, dimension(:, :) :: a\n", "", &block )
+    assert_subprocess_output( "allocate(a(5:15, n1))\n", "", &block2 )
+    assert_subprocess_output( "deallocate(a)\n", "", &block3 )
+    set_lang(C)
+    assert_subprocess_output( "int32_t * a;\n", "", &block )
+    assert_subprocess_output( "a = (int32_t *)malloc((sizeof(int32_t)) * (n1)*(11));\n", "", &block2 )
+    assert_subprocess_output( "free(a);\n", "", &block3 )
+  end
+
+  def test_decl_int_array_allocate
+    n1 = Int("n1")
+    arr = Int(:a, :dim => [Dim(5,15), Dim(n1)], :allocate => :heap, :align => 32)
+    block = lambda { decl arr }
+    block2 = lambda { pr arr.alloc(nil, 32) }
+    block3 = lambda { pr arr.dealloc }
+    set_lang(FORTRAN)
+    assert_subprocess_output( "integer(kind=4), allocatable, dimension(:, :) :: a\n", "", &block )
+    assert_subprocess_output( "allocate(a(5:15, n1))\n", "", &block2 )
+    assert_subprocess_output( "deallocate(a)\n", "", &block3 )
+    set_lang(C)
+    assert_subprocess_output( "int32_t * a;\n", "", &block )
+    assert_subprocess_output( "posix_memalign( &a, 32, (sizeof(int32_t)) * (n1)*(11));\n", "", &block2 )
+    assert_subprocess_output( "free(a);\n", "", &block3 )
+  end
+
 end
