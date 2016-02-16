a826fffd7548b5600c0c4d8f8d260a859e4ce8bf
swhatelse <stevenwhatelse@hotmail.fr>
2016-02-16 14:16:11 +0100

    Removed factor 1000000000

--- optimizer_benchmarks/bench_optimizer.rb
+++ optimizer_benchmarks/bench_optimizer.rb
@@ -7,12 +7,12 @@ oracle = YAML::load( File::read(ARGV[1]) )
 
 optimizer = BruteForceOptimizer::new( opt_space )
 puts optimizer.optimize { |opts|
-  oracle[opts]*1000000000
+  oracle[opts]
 }
 puts optimizer.experiments
 
 optimizer = GeneticOptimizer::new( opt_space )
 puts optimizer.optimize(:generations_limit => 10, :evolution_types => [Darwinning::EvolutionTypes::MutativeReproduction.new(mutation_rate: 0.10) ]) { |opts|
-  oracle[opts]*1000000000
+  oracle[opts]
 } 
 puts optimizer.experiments
