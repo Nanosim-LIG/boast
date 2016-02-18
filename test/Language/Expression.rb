d26aced283fbc1eabbd35e8d6c4e343d0754c3d2
Brice Videau <brice.videau@imag.fr>
2016-02-18 14:58:03 +0100

    Added test for Power and logical operations.

--- test/Language/Expression.rb
+++ test/Language/Expression.rb
@@ -3,15 +3,39 @@ require 'BOAST'
 include BOAST
 require_relative '../helper'
 
+def pow(a, b)
+  return a**b
+end
+
 class TestExpression < Minitest::Test
 
   def test_arithmetic
     exp = lambda { |a,b,c,d,e,f,g,h|
-      a + +b + -(c * d)/e +g*h
+      a + +b + -(c * d)/e +g**h
     }
     vals = 8.times.collect { rand(100) + 1 }
     vals_var = vals.collect(&:to_var)
-    assert_equal( exp.call(*vals), eval(exp.call(*vals_var).to_s) )
+    [FORTRAN,C,CL,CUDA].each { |l|
+      set_lang(l)
+      assert_equal( exp.call(*vals), eval(exp.call(*vals_var).to_s) )
+    }
+  end
+
+  def test_logic
+    ruby_exp = lambda { |a,b,c,d,e,f,g,h|
+      a < b || b >= c + a && !(d - e == g + h)
+    }
+    boast_exp = lambda { |a,b,c,d,e,f,g,h|
+      a < b |  b >= c + a &  !(d - e == g + h)
+    }
+    vals = 8.times.collect { rand(10) }
+    vals_var = vals.collect(&:to_var)
+    set_lang(FORTRAN)
+    assert_equal( ruby_exp.call(*vals), eval(boast_exp.call(*vals_var).to_s.gsub(".and.","&&").gsub(".or.","||").gsub(".not.", "!")))
+    [C,CL,CUDA].each { |l|
+      set_lang(l)
+      assert_equal( ruby_exp.call(*vals), eval(boast_exp.call(*vals_var).to_s) )
+    }
   end
 
 end
