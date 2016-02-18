80e0c420bcc638f89e43011fc193c91b7ffbcef3
Brice Videau <brice.videau@imag.fr>
2016-02-18 15:17:12 +0100

    More test for Boolean expressions.

--- test/Language/Expression.rb
+++ test/Language/Expression.rb
@@ -26,15 +26,25 @@ class TestExpression < Minitest::Test
       a < b || b >= c + a && !(d - e == g + h)
     }
     boast_exp = lambda { |a,b,c,d,e,f,g,h|
-      a < b |  b >= c + a &  !(d - e == g + h)
+      (a < b) | ( (b >= c + a) &  (!(d - e == g + h) ) )
+    }
+    boast_exp2 = lambda { |a,b,c,d,e,f,g,h|
+      (a < b).or( (b >= c + a).and(!(d - e == g + h) ) )
+    }
+    boast_exp3 = lambda { |a,b,c,d,e,f,g,h|
+      Or( a < b, And( b >= c + a, !(d - e == g + h) ) )
     }
     vals = 8.times.collect { rand(10) }
     vals_var = vals.collect(&:to_var)
     set_lang(FORTRAN)
-    assert_equal( ruby_exp.call(*vals), eval(boast_exp.call(*vals_var).to_s.gsub(".and.","&&").gsub(".or.","||").gsub(".not.", "!")))
+    assert_equal( ruby_exp.call(*vals), eval( boast_exp.call(*vals_var).to_s.gsub(".and.","&&").gsub(".or.","||").gsub(".not.", "!")))
+    assert_equal( ruby_exp.call(*vals), eval(boast_exp2.call(*vals_var).to_s.gsub(".and.","&&").gsub(".or.","||").gsub(".not.", "!")))
+    assert_equal( ruby_exp.call(*vals), eval(boast_exp3.call(*vals_var).to_s.gsub(".and.","&&").gsub(".or.","||").gsub(".not.", "!")))
     [C,CL,CUDA].each { |l|
       set_lang(l)
-      assert_equal( ruby_exp.call(*vals), eval(boast_exp.call(*vals_var).to_s) )
+      assert_equal( ruby_exp.call(*vals), eval( boast_exp.call(*vals_var).to_s) )
+      assert_equal( ruby_exp.call(*vals), eval(boast_exp2.call(*vals_var).to_s) )
+      assert_equal( ruby_exp.call(*vals), eval(boast_exp3.call(*vals_var).to_s) )
     }
   end
 
