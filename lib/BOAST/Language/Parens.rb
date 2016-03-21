277f1487f33766c90951935ba0f57a9f15fafad0
Brice Videau <brice.videau@imag.fr>
2016-03-21 14:25:11 +0100

    Added a mechanism to type registered func calls.

--- lib/BOAST/Language/Parens.rb
+++ lib/BOAST/Language/Parens.rb
@@ -2,10 +2,16 @@ module BOAST
 
   module_function
 
-  def register_funccall(name)
+  FUNCCALLS = {}
+
+  def register_funccall(name, options = {})
+    sym = name.to_sym
+    FUNCCALLS[sym] = {}
+    FUNCCALLS[sym][:parameters] = options[:parameters]
+    FUNCCALLS[sym][:returns] = options[:returns]
     s =<<EOF
     def self.#{name}(*args)
-      return FuncCall("#{name}", *args)
+      return FuncCall(#{sym.inspect}, *args#{options[:returns] ? ", returns: FUNCCALLS[#{sym.inspect}][:returns]" : ""})
     end
 EOF
     eval s
