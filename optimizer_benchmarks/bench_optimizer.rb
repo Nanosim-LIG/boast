a8a2ee6f9756df3f3f32af0fef9fef94c98f1aea
swhatelse <stevenwhatelse@hotmail.fr>
2016-06-28 13:41:03 +0200

    Added algo selection

--- optimizer_benchmarks/bench_optimizer.rb
+++ optimizer_benchmarks/bench_optimizer.rb
@@ -19,8 +19,13 @@ end
 
 
 options = {}
+
 opt_parser = OptionParser.new { |opts|
   opts.banner = "Usage: bench_optimizer.rb parameters.yaml data.yaml [options]"
+
+  opts.on("-aVAL", "--algorithm=VAL", "Specify the algorithms used: GA, BR, BOTH") { |n|
+    options[:algo] = n
+  }
 
   opts.on("-eVAL", "--elitism=VAL", Integer, "Specify the elitism value for the genetic algorithm") { |n|
     options[:elitism] = n
@@ -56,6 +61,8 @@ opt_parser = OptionParser.new { |opts|
   }
 }.parse!
 
+options[:algo] = options.fetch(:algo,"BOTH")
+
 opt_space = OptimizationSpace::new( YAML::load( File::read(ARGV[0]) ) )
 oracle = YAML::load( File::read(ARGV[1]) )
 
@@ -79,35 +86,39 @@ oracle = YAML::load( File::read(ARGV[1]) )
 
 opt_space.checkers.push checker
 
-optimizer = BruteForceOptimizer::new( opt_space )
-# puts optimizer.optimize { |opts|
-#   oracle[opts]
-# }
-# puts optimizer.experiments
-
-optimizer = GeneticOptimizer::new( opt_space )
-exploration_res = {}
-# best = optimizer.optimize(:generations_limit => 10, :evolution_types => [Darwinning::EvolutionTypes::MutativeReproduction.new(mutation_rate: 0.10) ]) { |opts|
-best = optimizer.optimize(:generations_limit => options.fetch(:generations_limit,10), 
-                          :evolution_types => [Darwinning::EvolutionTypes::MutativeReproduction.new(mutation_rate: options.fetch(:mutation_rate,0.10))],
-                          :population_size => options.fetch(:population_size, 20),
-                          :elitism => options.fetch(:elitism,1),
-                          :twin_removal => options.fetch(:twin_removal,true)
-                          ) { |opts|
-  exploration_res[opts] = oracle[opts]
-  oracle[opts]
-} 
-puts best
-puts optimizer.experiments
-
-if options[:result_file]
-  File::open( options[:result_file], "a") { |f|
-    f.print YAML::dump({best[0] => best[1]})
+if options[:algo] == "BOTH" or options[:algo] == "BR"  
+  optimizer = BruteForceOptimizer::new( opt_space )
+  puts optimizer.optimize { |opts|
+    oracle[opts]
   }
-end
+  puts optimizer.experiments
+end 
 
-if options[:log_file]
-  File::open( options[:log_file], "w") { |f|
-    f.print YAML::dump(exploration_res)
-  }
+if options[:algo] == "BOTH" or options[:algo] == "GA"  
+  optimizer = GeneticOptimizer::new( opt_space )
+  exploration_res = {}
+  # best = optimizer.optimize(:generations_limit => 10, :evolution_types => [Darwinning::EvolutionTypes::MutativeReproduction.new(mutation_rate: 0.10) ]) { |opts|
+  best = optimizer.optimize(:generations_limit => options.fetch(:generations_limit,10), 
+                            :evolution_types => [Darwinning::EvolutionTypes::MutativeReproduction.new(mutation_rate: options.fetch(:mutation_rate,0.10))],
+                            :population_size => options.fetch(:population_size, 20),
+                            :elitism => options.fetch(:elitism,1),
+                            :twin_removal => options.fetch(:twin_removal,true)
+                            ) { |opts|
+    exploration_res[opts] = oracle[opts]
+    oracle[opts]
+  } 
+  puts best
+  puts optimizer.experiments
+
+  if options[:result_file]
+    File::open( options[:result_file], "a") { |f|
+      f.print YAML::dump({best[0] => best[1]})
+    }
+  end
+
+  if options[:log_file]
+    File::open( options[:log_file], "w") { |f|
+      f.print YAML::dump(exploration_res)
+    }
+  end
 end
