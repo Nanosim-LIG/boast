c25aa489b867e2feb816818aaeec929ae0fc1f84
Luís Felipe Garlet Millani <lfgmillani@gmail.com>
2016-07-27 00:09:22 +0200

    add powercap energy probe

--- lib/BOAST/Runtime/EnergyProbe.rb
+++ lib/BOAST/Runtime/EnergyProbe.rb
@@ -1,4 +1,122 @@
 module BOAST
+  module PowercapProbe
+    extend PrivateStateAccessor
+    module_function
+    def header
+      get_output.puts "#include <stdio.h>"
+      get_output.puts "#include <stdint.h>"
+    end
+    def decl
+      get_output.puts "char **_boast_energy_files = 0;"
+      get_output.puts "char **_boast_energy_names = 0;"
+      get_output.puts "uint64_t *_boast_energy_0  = 0;"
+      get_output.puts "uint64_t *_boast_energy_1  = 0;"
+      get_output.puts "int _boast_energy_nsensors = 0;"
+    end
+    def configure
+      get_output.print <<EOF
+{
+  char buf[64];
+  char path[64];
+  char *s;
+  FILE *f;
+  int nproc;
+  int i;
+  if( _boast_energy_nsensors ){
+    free(_boast_energy_files);
+    for(i=0; i < _boast_energy_nsensors; ++i)
+      free(_boast_energy_names[i]);
+    free(_boast_energy_names);
+    _boast_energy_nsensors = 0;
+  }
+  _boast_energy_files = malloc(1);
+  _boast_energy_names = malloc(1);
+
+  for(nproc = 0; ; ++nproc){
+    sprintf(path,"/sys/devices/virtual/powercap/intel-rapl/intel-rapl:%d",nproc);
+    sprintf(buf,"%s/energy_uj",path);
+    f = fopen(buf, "rt");
+    if(!f)
+      break;
+    i = 0;
+    do{
+      fclose(f);
+      ++_boast_energy_nsensors;
+      _boast_energy_files = realloc(_boast_energy_files, _boast_energy_nsensors * sizeof(*_boast_energy_files));
+      _boast_energy_names = realloc(_boast_energy_names, _boast_energy_nsensors * sizeof(*_boast_energy_names));
+      _boast_energy_files[_boast_energy_nsensors-1] = malloc(128);
+      _boast_energy_names[_boast_energy_nsensors-1] = malloc( 16);
+      s = _boast_energy_names[_boast_energy_nsensors-1];
+      sprintf(_boast_energy_files[_boast_energy_nsensors-1],buf);
+      sprintf(buf,"%s/name",path);
+      f = fopen(buf, "r");
+      buf[fread(buf, 1, sizeof(buf), f)-1] = 0;
+      fclose(f);
+      sprintf(s, "%d.%s", nproc, buf);
+
+      sprintf(path,"/sys/devices/virtual/powercap/intel-rapl/intel-rapl:%d/intel-rapl:%d:%d",nproc,nproc,i++);
+      sprintf(buf,"%s/energy_uj",path);
+      f = fopen(buf, "rt");
+    }while(f);
+  }
+  if( ! _boast_energy_nsensors ){
+    free( _boast_energy_files );
+    free( _boast_energy_names );
+  }else{
+    _boast_energy_0 = malloc(_boast_energy_nsensors * sizeof(*_boast_energy_0));
+    _boast_energy_1 = malloc(_boast_energy_nsensors * sizeof(*_boast_energy_1));
+  }
+}
+EOF
+    end
+    def start
+      get_output.print <<EOF
+{
+  char buf[32];
+  FILE *f;
+  int i;
+  for(i = 0; i < _boast_energy_nsensors; ++i){
+    f = fopen(_boast_energy_files[i], "r");
+    fread(buf, 1, sizeof(buf), f);
+    fclose(f);
+    _boast_energy_0[i] = atoll(buf);
+  }
+}
+EOF
+    end
+    def stop
+      get_output.print <<EOF
+{
+  char buf[32];
+  FILE *f;
+  int i;
+  for(i = 0; i < _boast_energy_nsensors; ++i){
+    f = fopen(_boast_energy_files[i], "r");
+    fread(buf, 1, sizeof(buf), f);
+    fclose(f);
+    _boast_energy_1[i] = atoll(buf);
+  }
+}
+EOF
+    end
+    def compute
+      get_output.print <<EOF
+{
+  VALUE results;
+  int i;
+  results = rb_hash_new();
+  for(i=0; i < _boast_energy_nsensors; ++i){
+    rb_hash_aset(results, ID2SYM(rb_intern(_boast_energy_names[i])), rb_float_new((_boast_energy_1[i] - _boast_energy_0[i]) * 1e-6));
+  }
+  rb_hash_aset(_boast_stats, ID2SYM(rb_intern("energy")), results);
+}
+EOF
+    end
+    def is_available
+      [] != Dir.glob( '/sys/class/powercap/intel-rapl:0:0' )
+    end
+  end
+
   module RedfstProbe
     extend PrivateStateAccessor
     module_function
@@ -90,7 +208,9 @@ EOF
       [] != ENV['LIBRARY_PATH'].split(':').inject([]){|mem, x| []!=mem ? mem : Dir.glob(x+'/libeml.so')}
     end
   end
-  if RedfstProbe.is_available
+  if PowercapProbe.is_available
+    EnergyProbe = PowercapProbe
+  elsif RedfstProbe.is_available
     EnergyProbe = RedfstProbe
   elsif EmlProbe.is_available
     EnergyProbe = EmlProbe
