978f72253d724e090fe6be44c9b3b0fb3971b44a
Luís Felipe Garlet Millani <lfgmillani@gmail.com>
2016-07-27 00:06:47 +0200

    change working of energy probes

--- lib/BOAST/Runtime/EnergyProbe.rb
+++ lib/BOAST/Runtime/EnergyProbe.rb
@@ -1,92 +1,60 @@
 module BOAST
-  module Energy
-    class Redfst
-#      require 'inline'
-#      inline(:C) do |b|
-#        b.add_compile_flags '-lredfst'
-#        b.include '"redfst.h"'
-#        b.c 'void energy_init(){}'
-#      end
-      def initialize
-#        energy_init
-        true
-      end
-      def start
-#        self.asdqwe
-      end
-      def stop
-      end
-      def compute
-      end
-    end
-    @@impl = Redfst.new
+  module RedfstProbe
+    extend PrivateStateAccessor
     module_function
-    def config_validate
+    def header
+      get_output.puts "#include <redfst.h>"
     end
-    def config_read
+    def decl
+      get_output.puts "redfst_dev_t *_boast_energy=0;"
     end
-    def get
-    end
-    def initialize
+    def configure
+      get_output.puts "redfst_init();"
     end
     def start
-#      puts @@impl
-#     @impl.start
+      get_output.puts "redfst_reset();"
     end
     def stop
-#     impl.stop
+      get_output.puts "_boast_energy = redfst_get(_boast_energy);"
     end
     def compute
+      get_output.print <<EOF
+{
+  VALUE results;
+  double pkg, pp0, dram;
+  char *s;
+  int i;
+  pkg = pp0 = dram = 0;
+  results = rb_hash_new();
+  for(i=0; i < _boast_energy->count; ++i){
+    rb_hash_aset(results, ID2SYM(rb_intern(_boast_energy->name[i])), rb_float_new(_boast_energy->energy[i]));
+    s = _boast_energy->name[i];
+    while('.'!=*s++)
+      ;
+    while('.'!=*s++)
+      ;
+    if(!strcmp("pkg",s))
+      pkg += _boast_energy->energy[i];
+    else if(!strcmp("pp0",s))
+      pp0 += _boast_energy->energy[i];
+    else if(!strcmp("dram",s))
+      dram += _boast_energy->energy[i];
+  }
+  rb_hash_aset(results, ID2SYM(rb_intern("total.pkg" )), rb_float_new(pkg));
+  rb_hash_aset(results, ID2SYM(rb_intern("total.pp0" )), rb_float_new(pp0));
+  rb_hash_aset(results, ID2SYM(rb_intern("total.dram")), rb_float_new(dram));
+  rb_hash_aset(results, ID2SYM(rb_intern("total"     )), rb_float_new(pkg+dram));
+  rb_hash_aset(_boast_stats, ID2SYM(rb_intern("energy")), results);
+}
+EOF
+		end
+    def is_available
+      [] != ENV['LIBRARY_PATH'].split(':').inject([]){|mem, x| []!=mem ? mem : Dir.glob(x+'/libredfst.so')}
     end
   end
-
-#  module EnergyRedfstProbe
-#    extend PrivateStateAccessor
-#    module_function
-#    def header
-#      get_output.print "#include <redfst.h>\n"
-#    end
-#    def decl
-#      get_output.print "double *energyData;\n"
-#    end
-#    def configure
-#      get_output.print '
-#//      rb_hash_aset(_boast_stats,ID2SYM(rb_intern("energy")), rb_float_new(1.0));
-#      redfst_init();
-#      energyData = malloc((3 * redfst_ncpus() + 1) * sizeof(*energyData));
-#      '
-#    end
-#    def start
-#      get_output.print "redfst_reset();\n"
-#    end
-#    def stop
-#      get_output.print "redfst_get_all(energyData);\n"
-#    end
-#    def compute
-#      get_output.print '{
-#        VALUE results;
-#        VALUE cpu;
-#        double pkg, pp0, mem, tot;
-#        int i;
-#        results = rb_hash_new();
-#        tot = 0;
-#        for(i=0; i < redfst_ncpus(); ++i){
-#          cpu = rb_hash_new();
-#          pkg = energyData[3*i + 0];
-#          pp0 = energyData[3*i + 1];
-#          mem = energyData[3*i + 2];
-#          tot += pkg + mem;
-#          rb_hash_aset(cpu, rb_str_new2("pkg"), rb_float_new(pkg));
-#          rb_hash_aset(cpu, rb_str_new2("pp0"), rb_float_new(pp0));
-#          rb_hash_aset(cpu, rb_str_new2("mem"), rb_float_new(mem));
-#          rb_hash_aset(cpu, rb_str_new2("total"), rb_float_new(pkg + mem));
-#          rb_hash_aset(results, INT2FIX(i), cpu);
-#        }
-#        rb_hash_aset(results, rb_str_new2("total"), rb_float_new(tot));
-#        rb_hash_aset(_boast_stats,ID2SYM(rb_intern("energy")), results);
-#      }'
-#    end
-#  end
-#  EnergyProbe = EnergyRedfstProbe
+  if RedfstProbe.is_available
+    EnergyProbe = RedfstProbe
+  else
+    EnergyProbe = 'no energy probe available'
+  end
 end
-
