696dff3c5cc313240d16f9974b47651cd1b35a0c
Luís Felipe Garlet Millani <lfgmillani@gmail.com>
2016-07-27 00:08:41 +0200

    add eml energy probe

--- lib/BOAST/Runtime/EnergyProbe.rb
+++ lib/BOAST/Runtime/EnergyProbe.rb
@@ -52,8 +52,48 @@ EOF
       [] != ENV['LIBRARY_PATH'].split(':').inject([]){|mem, x| []!=mem ? mem : Dir.glob(x+'/libredfst.so')}
     end
   end
+
+  module EmlProbe
+    extend PrivateStateAccessor
+    module_function
+    def header
+      get_output.puts "#include <eml.h>"
+    end
+    def decl
+      get_output.puts "emlData_t **_boast_energy=0;";
+      get_output.puts "size_t _boast_energy_count=0;";
+    end
+    def configure
+      get_output.puts "emlInit();"
+      get_output.puts "emlDeviceGetCount(&_boast_energy_count);"
+      get_output.puts "_boast_energy = malloc(_boast_energy_count*sizeof(*_boast_energy));"
+    end
+    def start
+      get_output.puts "emlStart();";
+    end
+    def stop
+      get_output.puts "emlStop(_boast_energy);";
+    end
+    def compute
+      get_output.print <<EOF
+{
+  VALUE results;
+  double consumed;
+  results = rb_hash_new();
+  emlDataGetConsumed(_boast_energy[0], &consumed);
+  rb_hash_aset(results, ID2SYM(rb_intern("total" )), rb_float_new(consumed));
+  rb_hash_aset(_boast_stats, ID2SYM(rb_intern("energy")), results);
+}
+EOF
+    end
+    def is_available
+      [] != ENV['LIBRARY_PATH'].split(':').inject([]){|mem, x| []!=mem ? mem : Dir.glob(x+'/libeml.so')}
+    end
+  end
   if RedfstProbe.is_available
     EnergyProbe = RedfstProbe
+  elsif EmlProbe.is_available
+    EnergyProbe = EmlProbe
   else
     EnergyProbe = 'no energy probe available'
   end
