9018f50ea86cc8494c0c01429fd9020b0b9beda5
Brice Videau <brice.videau@imag.fr>
2017-01-09 16:34:11 +0100

    Refined model naming to correspond to gcc.

--- lib/BOAST/Language/Architectures.rb
+++ lib/BOAST/Language/Architectures.rb
@@ -294,8 +294,8 @@ X86architectures ={"pentium2"=>["MMX"],
    "MMX"]}
   private_constant :X86architectures
   ARMarchitectures = {
-    "ARMv7" => ["NEON"],
-    "ARMv8" => ["ASIMD"]
+    "armv7-a" => ["NEON"],
+    "armv8-a" => ["ASIMD"]
   }
   private_constant :ARMarchitectures
 end
