7c8d98012f878a6a20cd30c6479e3e225ae509a4
Brice Videau <brice.videau@imag.fr>
2017-05-11 08:07:30 +0200

    spinlock not accessible on mac os.

--- lib/BOAST/Runtime/CoExecute.rb
+++ lib/BOAST/Runtime/CoExecute.rb
@@ -18,8 +18,12 @@ module BOAST
       #attach_function 'pthread_cond_wait', [ :cond, :mutex ], :int
       #attach_function 'pthread_cond_broadcast', [ :cond ], :int
 
-      attach_function 'pthread_spin_init', [ :spin, :int ], :int
-      attach_function 'pthread_spin_destroy', [ :spin ], :int
+      begin
+        attach_function 'pthread_spin_init', [ :spin, :int ], :int
+        attach_function 'pthread_spin_destroy', [ :spin ], :int
+      rescue NotFoundError => e
+        warn "spin functions not found"
+      end
     end
 
     def self.coexecute(kernels)
